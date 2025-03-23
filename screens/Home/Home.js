35910e9f3ed677f7d1367e3fcc63d846f9f820b5
evtaindra <evtapribadi@gmail.com>
2025-03-23 16:42:30 +0700

    Menambahkan Komponen Search dan FontAwesome

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -7,6 +7,7 @@ import Button from '../../components/Button/Button';
 import globalStyle from '../../assets/styles/globalStyle';
 import Tab from '../../components/Tab/Tab';
 import Badge from '../../components/Badge/Badge';
+import Search from '../../components/Search/Search';
 //import style from './style';
 
 const Home = () => {
@@ -14,10 +15,17 @@ const Home = () => {
     <SafeAreaView>
       <View style={globalStyle.backgroundWhite}>
         <Header title={'Mr Dojo'} type={1} />
+        <Search
+          onSearch={value => {
+            console.log(value);
+          }}
+        />
+
+        {/* <Header title={'Mr Dojo'} type={1} />
         <Tab title={'Highlight'} />
         <Tab title={'Highlight'} isInactive={true} />
         <Badge title={'Environment'} />
-        {/* <Button title={'Donasi'} />
+        <Button title={'Donasi'} />
         <Button title={'Donasi lagi'} isDisabled={true} /> */}
       </View>
     </SafeAreaView>
